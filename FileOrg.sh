dd8b3e82759ae487f6b7a08cb65b0dd01945d218
sudobyter-hub <sudobyter@gmail.com>
2021-05-30 01:58:52 +0300

    with loop to make it simpler

---
+++ FileOrg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+while : 
+do
+echo "Please enter path of dirctory source: "
+
+read src 
+
+echo "Please enter path of destenation derctoriy: "
+
+read dst
+
+echo "Please type the file extention like "png , mp4" :"
+
+read ext 
+
+find $src -type f -iname "*.$ext" -exec mv --backup=numbered -t $dst {} + 
+
+echo "Please note to stop press CTRL+C"
+
+done 
+
